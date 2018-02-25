96858b8398de7e36c03ef352db048d24be5e4a4a
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-02-25 13:05:55 +0000

    Creates API startpoints and end-points in url.py

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,17 +1,27 @@
-from django.conf.urls import url
+from .views.views import SnippetList, SnippetDetail, UserList, UserDetail
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views.views import SnippetList, SnippetDetail
-from .views.views import UserList, UserDetail
-from django.conf.urls import include
+from snippets import views
 
-urlpatterns = [
-    url(r'^snippets/$', SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', SnippetDetail.as_view()),
-    url(r'^users/$', UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)$', UserDetail.as_view()),
-    url(r'^api-auth/', include('rest_framework.urls')),
+# API endpoints
+urlpatterns = format_suffix_patterns([
     url(r'^$', views.api_root),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
-]
+    url(r'^snippets/$',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail')
+])
+
 
 urlpatterns = format_suffix_patterns(urlpatterns)
