8febd6503b06052effbe843eeb62ea14dbece909
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-02-25 13:41:34 +0000

    adds files with edits

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,37 +1,20 @@
-from .views.views import SnippetList, SnippetDetail, UserList, UserDetail
 from django.conf.urls import url, include
-from rest_framework import renderers
-from rest_framework.urlpatterns import format_suffix_patterns
-from snippets.views.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework.routers import DefaultRouter
+from snippets import views
+from rest_framework.schemas import get_schema_view
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
+schema_view = get_schema_view(title='Pastebin API')
 
-urlpatterns = format_suffix_patterns([
-    url(r'^$', api_root),
-    url(r'^snippets/$', snippet_list, name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
-])
+urlpatterns = [
+    url(r'^schema/$', schema_view),
+    ...
+]
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
-
-# urlpatterns = format_suffix_patterns(urlpatterns)
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    url(r'^', include(router.urls))
+]
