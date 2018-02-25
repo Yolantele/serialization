2c684e3c2110578348f01c09ecef88e38fe3aca2
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-02-25 11:41:31 +0000

    Creates custom permissions object IsOwnerOrReadOnly  for SnippetsDetail class

---
+++ tutorial/snippets/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions 
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    #custom permission to allow owners of object to edit it
+
+    def has_object_permission(self, request, view, object)
+        #read permissions are allowed to any request, so we will allow GET, HEAD or OPTIONS requests:
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        #write permissions are only allowed to the owner of snippet. 
+        return obj.owner == request.user
+        
---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views.views import SnippetList, SnippetDetail
+from .views.views import UserList, UserDetail
+from django.conf.urls import include
+
+urlpatterns = [
+    url(r'^snippets/$', SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', SnippetDetail.as_view()),
+    url(r'^users/$', UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)$', UserDetail.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls')),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
